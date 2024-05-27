30d78f03741634913db7d1f61d7b9522d9457993
andrew hulme <andrew_hulme04@hotmail.com>
2024-05-27 18:17:50 +1000

    Completed section

--- test.js
+++ test.js
@@ -1,3 +1,32 @@
-theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+function Person(name) {
+  this.name = name;
+}
 
-console.log(theHobbit.info());
+Person.prototype.sayName = function() {
+  console.log(`Hello, I'm ${this.name}!`);
+};
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+}
+
+// Don't do this!
+// Use Object.setPrototypeOf(Player.prototype, Person.prototype)
+Player.prototype = Person.prototype;
+
+function Enemy(name) {
+  this.name = name;
+  this.marker = '^';
+}
+
+// Not again!
+// Use Object.setPrototypeOf(Enemy.prototype, Person.prototype)
+Enemy.prototype = Person.prototype;
+
+Enemy.prototype.sayName = function() {
+  console.log('HAHAHAHAHAHA');
+};
+
+const carl = new Player('carl', 'X');
+carl.sayName(); // Uh oh! this logs "HAHAHAHAHAHA" because we edited the sayName function!
